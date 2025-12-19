bbba1613cda2ebb765044de68ca29385613749fd
itsarjunfr <itsarjunfr@gmail.com>
2025-12-19 15:43:51 +0800

    initial commit

---
+++ string operation.py
@@ -0,0 +1,13 @@
+str1 = input("Enter words:")
+print(str1[0])
+print(str1[-2])
+print(str1[1:4])
+print(str1[::2])
+print(len(str1))
+print(str1.upper())
+print(str1.lower())
+print(str1.swapcase())
+str2 = "World!"
+str3 = str1 + str2
+print(str3)
+print(str3[::-1])
